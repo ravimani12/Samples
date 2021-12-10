0912f9a2157bc7e71b35370d698dbb9d809763ca
ravimani12 <manikandan30796@gmail.com>
2021-12-10 20:16:35 +0530

    my pro

---
+++ src/org/samples/Samples.java
@@ -0,0 +1,11 @@
+package org.samples;
+
+public class Samples {
+private void empid() {
+System.out.println("emp id is 24");
+}
+public static void main(String[] args) {
+	Samples s = new Samples();
+	s.empid();
+}
+}
